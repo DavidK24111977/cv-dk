4d3140dde5b693a640957b17da78010318b6286f
DavidK <davidkips77@gmail.com>
2019-10-28 13:15:42 +0100

    amelioration anim

--- js/app.js
+++ js/app.js
@@ -3,13 +3,16 @@ $(document).ready(function(){
         $(".minus:eq(0)").hide();
         $(".plus:eq(0)").show();
         $(".experience .tohide").hide();
-        $(".career:eq(0) h2").css("top","0").css("left","5px");
+        // $(".career:eq(0) h2").css("top","0").css("left","5px");
+        $(".career:eq(0) h2").toggleClass("title-inside");
+        $(".career:eq(0) h2").toggleClass("title-outside");
     });
     $(".plus:eq(0)").click(function(){
         $(".minus:eq(0)").show();
         $(".plus:eq(0)").hide();
         $(".experience .tohide").show();
-        $(".career:eq(0) h2").css("top","-50px").css("left","-50px");
+        $(".career:eq(0) h2").toggleClass("title-inside");
+        $(".career:eq(0) h2").toggleClass("title-outside");
     });
 
 
@@ -17,25 +20,29 @@ $(document).ready(function(){
         $(".minus:eq(1)").hide();
         $(".plus:eq(1)").show();
         $(".skill .tohide").hide();
-        $(".career:eq(1) h2").css("top","0").css("left","5px");
+        $(".career:eq(1) h2").toggleClass("title-inside");
+        $(".career:eq(1) h2").toggleClass("title-outside");
     });
     $(".plus:eq(1)").click(function(){
         $(".minus:eq(1)").show();
         $(".plus:eq(1)").hide();
         $(".skill .tohide").show();
-        $(".career:eq(1) h2").css("top","-50px").css("left","-50px");
+        $(".career:eq(1) h2").toggleClass("title-inside");
+        $(".career:eq(1) h2").toggleClass("title-outside");
     });
 
     $(".minus:eq(2)").click(function(){
         $(".minus:eq(2)").hide();
         $(".plus:eq(2)").show();
         $(".training .tohide").hide();
-        $(".career:eq(2) h2").css("top","0").css("left","5px");
+        $(".career:eq(2) h2").toggleClass("title-inside");
+        $(".career:eq(2) h2").toggleClass("title-outside");
     });
     $(".plus:eq(2)").click(function(){
         $(".minus:eq(2)").show();
         $(".plus:eq(2)").hide();
         $(".training .tohide").show();
-        $(".career:eq(2) h2").css("top","-50px").css("left","-50px");
+        $(".career:eq(2) h2").toggleClass("title-inside");
+        $(".career:eq(2) h2").toggleClass("title-outside");
     });
 });
